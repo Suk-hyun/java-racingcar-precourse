5c22371eacfcd755ed3e6820aa4a704137ade76d
simba <shim9597@gmail.com>
2023-07-09 18:54:38 +0900

    feat: 자동차 이름과 시도횟수를 입력받는 기능 구현
    커밋 누락 부분

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,14 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String requestCarNameInput() {
+        return Console.readLine();
+    }
+
+    public static String requestTrialNumber() {
+        return Console.readLine();
+    }
+}
